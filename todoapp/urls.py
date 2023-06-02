5196d859579c277b979d5e4e9c15d63b099cf5ad
ytksato <ytk.sato@gmail.com>
2023-06-02 16:32:33 +0900

    削除ページ実装

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     # 関数ベースビューの書き方
@@ -14,4 +14,6 @@ urlpatterns = [
     path('task/create/', TaskCreate.as_view(), name='task-create'),
     # update
     path('task/update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    # delete
+    path('task/delete/<int:pk>', TaskDelete.as_view(), name='task-delete'),
 ]
