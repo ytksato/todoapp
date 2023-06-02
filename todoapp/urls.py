5c77df68ba099d30a889db1ac2587c6760aa35ec
ytksato <ytk.sato@gmail.com>
2023-06-02 15:08:15 +0900

    詳細ページの作成とリストからのリンク

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
     # 関数ベースビューの書き方
     #path('', views.taskList),
 
     # クラスベースビューの書き方
-    path('', TaskList.as_view(), name='tasks'),
+    path('', TaskList.as_view()),
+    # <int:pk>でidを取得(動的キー)
+    path('task/detail/<int:pk>', TaskDetail.as_view(), name='task-detail'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # クラスベースビュー
 from django.views.generic.list import ListView
 from todoapp.models import Task
+from django.views.generic.detail import DetailView
 
 # 関数ベースビュー
 # def taskList(request):
@@ -14,3 +15,8 @@ class TaskList(ListView):
     model = Task
     # テンプレートで使う変数名を指定
     context_object_name = 'tasks'
+
+# Taskの詳細ページ
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
