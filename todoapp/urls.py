d6a69c29b7974e700209ba875ed2b0087b8fc078
ytksato <ytk.sato@gmail.com>
2023-06-02 11:16:20 +0900

    クラスベースビューに書き換え

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import TaskList
 
 urlpatterns = [
-    path('', views.taskList),
+    # 関数ベースビューの書き方
+    #path('', views.taskList),
+
+    # クラスベースビューの書き方
+    path('', TaskList.as_view(), name='tasks'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def taskList(request):
-    return HttpResponse('Hello World')
+# クラスベースビュー
+from django.views.generic.list import ListView
+from todoapp.models import Task
+
+# 関数ベースビュー
+# def taskList(request):
+#     return HttpResponse('Hello World')
+
+# クラスベースビュー
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
